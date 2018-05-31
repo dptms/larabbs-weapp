bfe1dceb803554e981f84c1fd32b6e9493a231f2
dp~ <457509824@qq.com>
2018-05-31 17:07:05 +0800

    unread notification

---
+++ src/mixins/unreadCount.js
@@ -0,0 +1,44 @@
+import wepy from 'wepy'
+
+export default class UnreadCount extends wepy.mixin {
+    data = {
+        // 轮训
+        interval: null,
+        // 未读消息
+        unreadCount: 0
+    }
+
+    // 页面显示
+    onShow() {
+        this.updateUnreadCount()
+        this.interval = setInterval(() => {
+            this.updateUnreadCount()
+        }, 30000)
+    }
+
+    // 页面隐藏
+    onHide() {
+        // 关闭轮训
+        clearInterval(this.interval)
+    }
+
+    // 设置未读消息数
+    updateUnreadCount() {
+        // 从全局获取未读消息数
+        this.unreadCount = this.$parent.globalData.unreadCount
+        this.$apply()
+
+        if (this.unreadCount) {
+            // 设置 bagge
+            wepy.setTabBarBadge({
+                index: 1,
+                text: this.unreadCount.toString()
+            })
+        } else {
+            // 移除 badge
+            wepy.removeTabBarBadge({
+                index: 1,
+            })
+        }
+    }
+}
